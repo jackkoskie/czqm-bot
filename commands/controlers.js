b47ed036802d92ab4e5ae7da97042513af5cde21
GoldenXLence <jack@koskie.ca>
2021-01-04 14:03:21 -0500

    Permissions Update v0.2

--- commands/controlers.js
+++ commands/controlers.js
@@ -1,11 +1,15 @@
 module.exports = {
     name: 'controlers',
     description: 'Lists interested controlers for a certain event',
-    execute(message, args, config, Discord, mongoose, eventSchema, client) {
-        async function controlers(message) {
-            var doc = await eventSchema.findOne({ "_id": args[1] });
-            message.channel.send(doc.people.join(' '));
+    execute(message, args, config, Discord, mongoose, eventSchema, client, permissions) {
+        if (permissions.event.includes(message.member.user.id)) {
+            async function controlers(message) {
+                var doc = await eventSchema.findOne({ "_id": args[1] });
+                message.channel.send(doc.people.join(' '));
+            }
+            controlers(message)
+        } else {
+            message.channel.send('Sorry! You do not have permission to use that command')
         }
-        controlers(message)
     },
 };
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { measureMemory } = require('vm');
 const mongoose = require('mongoose');
 const { dburl } = require('./database.json');
 const eventSchema = require('./models/event')
+const permissions = require('./permissions.json');
 
 // Tells the console when the bot has logged on
 client.on('ready', () => {
@@ -33,23 +34,23 @@ client.on('message', message => {
 
         switch (args[0]) {
             case 'ping':
-                client.commands.get('ping').execute(message, args);
+                client.commands.get('ping').execute(message, args, config, Discord, mongoose, eventSchema, client, permissions);
                 break;
 
             case 'event':
             case 'ev':
-                client.commands.get('event').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('event').execute(message, args, config, Discord, mongoose, eventSchema, client, permissions);
                 break;
 
             case 'roster':
             case 'ro':
-                client.commands.get('roster').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('roster').execute(message, args, config, Discord, mongoose, eventSchema, client, permissions);
                 break;
             case 'control':
-                client.commands.get('control').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('control').execute(message, args, config, Discord, mongoose, eventSchema, client, permissions);
                 break;
             case 'controlers':
-                client.commands.get('controlers').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('controlers').execute(message, args, config, Discord, mongoose, eventSchema, client, permissions);
                 break;
         }
     }
