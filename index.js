254becaaf5c317d194f1ba8dc687416918c3f3eb
GoldenXLence <jack@koskie.ca>
2021-01-28 09:22:09 -0500

    DOTENV Update v1.1.0

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // All the requires
+const dotenv = require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token } = require('./token.json');
 const fs = require('fs')
-const config = require('./config.json');
 const { measureMemory } = require('vm');
 const mongoose = require('mongoose');
-const { dburl } = require('./database.json');
 const eventSchema = require('./models/event')
 
 // Tells the console when the bot has logged on
@@ -26,36 +24,36 @@ for (const file of commandFiles) {
 
 // Reads Incoming Messages
 client.on('message', message => {
-    if (!message.content.startsWith(`${config.prefix}`)) {
+    if (!message.content.startsWith(process.env.COMMAND_PREFIX)) {
         return;
     } else {
-        let args = message.content.substring(config.prefix.length).split(" ");
+        let args = message.content.substring(process.env.COMMAND_PREFIX.length).split(" ");
 
         switch (args[0]) {
             case 'ping':
-                client.commands.get('ping').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('ping').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
 
             case 'event':
             case 'ev':
-                client.commands.get('event').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('event').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
 
             case 'roster':
             case 'ro':
-                client.commands.get('roster').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('roster').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
             case 'control':
-                client.commands.get('control').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('control').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
             case 'controlers':
-                client.commands.get('controlers').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('controlers').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
             case 'help':
-                client.commands.get('help').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('help').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
             case 'metar':
-                client.commands.get('metar').execute(message, args, config, Discord, mongoose, eventSchema, client);
+                client.commands.get('metar').execute(message, args, process.env, Discord, mongoose, eventSchema, client);
                 break;
         }
     }
@@ -63,7 +61,7 @@ client.on('message', message => {
 
 // Logs the bot in to the Database
 try {
-    mongoose.connect(dburl, {
+    mongoose.connect(process.env.DB_URL, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useUnifiedTopology: true
@@ -74,7 +72,7 @@ try {
 
 // Logs the bot in to Discord
 try {
-    client.login(token);
+    client.login(process.env.DISCORD_TOKEN);
 } catch (err) {
     console.error(`Error connecting to Discord. Received the following error:\n${err}`);
 }
